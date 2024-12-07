0992848b8bcadb54c51c1133fc6a51b5b128421a
SHUVO HALDER <samshuvo00@gmail.com>
2024-12-07 21:05:04 +0600

    using package and adding this in main function
    adding this in main function and create new usage function on main.py file

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-#########################################################
-#! /usr/bin/python3                                     #
-# -=- coding: utf-8 -=-                                 #
-#                                                       #
-# python 3.3.2+ Python Youtube Downloader Script v.1    #
-# by cybsam                                             #
-# only for legal purpose                                #
-# https://github.com/cybsam/pytube-yt-downloader.git    #
-#########################################################
+#############################################################
+#! /usr/bin/python3                                         #
+# -=- coding: utf-8 -=-                                     #
+#                                                           #
+# python 3.3.2+ Python Youtube Downloader Script v.1        #
+# by cybsam                                                 #
+# only for legal purpose                                    #
+# https://github.com/shuvo-halder/pytube-yt-downloader.git  #
+#############################################################
 from pytube import YouTube
 import os
 import time
 import subprocess
+from queue import Queue
+from optparse import OptionParser
+import sys,socket,threading,logging,urllib.request,random
+
+def usage():
+    print(''' \033[92m Youtube video downloader v.1
+    https://github.com/shuvo-halder/pytube-yt-downloader.git
+    It is the end user's responsibility to obey all applicable laws.
+    usege:
+        python3 main.py
+        URL:
+
+
+      \033[0m''')
+      
 
 def Download(link):
     youtubeObject = YouTube(link)
@@ -31,6 +46,7 @@ def Download(link):
         print("An error has occurred")
     print("Download is completed successfully")
 
-
-link = input("Enter the YouTube video URL: ")
-Download(link)
+if __name__ == '__main__':
+    usage()
+    link = input("Enter the YouTube video URL: ")
+    Download(link)
